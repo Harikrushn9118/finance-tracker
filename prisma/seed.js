223cfc74ca61ecf01b3b5b2bfa7b9f446701abaa
HK Patel <hkssoa@gmail.com>
2025-11-26 10:09:48 +0530

    add database seed script for default categories

---
+++ prisma/seed.js
@@ -0,0 +1,23 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      name: "HK Patel",
+      email: "hk@example.com",
+      password: "123456"
+    }
+  })
+
+  console.log("Seed done!")
+}
+
+main()
+  .then(() => prisma.$disconnect())
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
